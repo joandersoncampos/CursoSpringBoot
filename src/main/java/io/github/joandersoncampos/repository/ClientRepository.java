546b09d4d7680d6a7298034c93ec805505f5e029
Joanderson Campos <joanderson.ita@gmail.com>
2021-07-07 12:38:39 -0300

    Commit Inicial

---
+++ src/main/java/io/github/joandersoncampos/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package io.github.joandersoncampos.repository;
+
+import io.github.joandersoncampos.model.Client;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository {
+    public void persist(Client client){
+        // INSERT INTO CLIENT .....
+    }
+}
